f51d4eef137fa2ef217ef2c7feed72507c32a611
leobernardo05 <142163011+leobernardo05@users.noreply.github.com>
2024-09-16 18:45:16 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var duvidas = document.querySelectorAll(".duvida")
+
+duvidas.forEach(function (duvida) {
+    duvida.addEventListener('click', function () {
+        duvida.classList.toggle('ativa')
+    })
+
+
+})
+
+
+/*function nome(argumento) {
+    conteudo função
+}*/
